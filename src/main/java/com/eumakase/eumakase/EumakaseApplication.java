7184bf1d95cf3b9206a20635ac62aaedb67b3c67
이재영 <2jaebbang@naver.com>
2024-01-09 16:05:20 +0900

    first commit

---
+++ src/main/java/com/eumakase/eumakase/EumakaseApplication.java
@@ -0,0 +1,13 @@
+package com.eumakase.eumakase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EumakaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EumakaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eumakase/eumakase/EumakaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eumakase.eumakase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EumakaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
