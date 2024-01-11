dfa217e43dfd9b43e9c8645ef97506102616c1bb
2jaebbang <2jaebbang@naver.com>
2024-01-11 19:49:33 +0900

    feat:ChatGPT 프롬프트 입력하여 문장 단어 추출하는 API 추가

---
+++ src/main/java/com/eumakase/eumakase/controller/ChatGPTController.java
@@ -0,0 +1,38 @@
+package com.eumakase.eumakase.controller;
+
+import com.eumakase.eumakase.dto.common.ApiResponse;
+import com.eumakase.eumakase.dto.chatGPT.PromptRequestDto;
+import com.eumakase.eumakase.dto.chatGPT.PromptResponseDto;
+import com.eumakase.eumakase.service.ChatGPTService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+/**
+ * ChatGPT API
+ */
+@RestController
+@RequestMapping(value = "/api/v1/chatGpt")
+public class ChatGPTController {
+    private final ChatGPTService chatGPTService;
+
+    public ChatGPTController(ChatGPTService chatGPTService) {
+        this.chatGPTService = chatGPTService;
+    }
+
+    /**
+     * ChatGPT 프롬프트 입력하여 문장 단어 추출
+     */
+    @PostMapping("/question")
+    public ResponseEntity<ApiResponse<PromptResponseDto>> sendPrompt(@RequestBody PromptRequestDto promptRequestDto) {
+        try {
+            PromptResponseDto promptResponse = chatGPTService.sendPrompt(promptRequestDto);
+            return ResponseEntity.ok(ApiResponse.success(promptResponse));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(ApiResponse.error("생성 실패했습니다."));
+        }
+    }
+}
