9dd722e235fccac447009db3461785869ab30150
이재영 <2jaebbang@naver.com>
2024-01-22 09:04:47 +0900

    fix: signup controller 누락부분 추가

--- src/main/java/com/eumakase/eumakase/controller/AuthController.java
+++ src/main/java/com/eumakase/eumakase/controller/AuthController.java
@@ -1,15 +1,38 @@
+package com.eumakase.eumakase.controller;
+
+import com.eumakase.eumakase.common.dto.ApiResponse;
+import com.eumakase.eumakase.dto.auth.SignUpRequestDto;
+import com.eumakase.eumakase.dto.auth.SignUpResponseDto;
+import com.eumakase.eumakase.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Auth API
+ */
+@RestController
+@RequestMapping(value = "/api/v1/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
     /**
-     * Login
+     * Sign Up
      */
-//    @PostMapping("/login")
-//    public ResponseEntity<ApiResponse<LoginResponseDto>> createDiary(@RequestBody @Validated LoginRequestDto loginRequestDto) {
-//        try {
-//            LoginResponseDto loginResponseDto = authService.login(loginRequestDto);
-//            return ResponseEntity.ok(ApiResponse.success("로그인에 성공했습니다.",loginResponseDto));
-//        } catch (Exception e) {
-//            return ResponseEntity
-//                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-//                    .body(ApiResponse.error("로그인에 실패했습니다."));
-//        }
-//    }
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponse<SignUpResponseDto>> signup(@Valid @RequestBody SignUpRequestDto signUpRequestDto) {
+        try {
+            SignUpResponseDto signUpResponseDto = authService.signUp(signUpRequestDto);
+            return ResponseEntity.ok(ApiResponse.success("회원가입에 성공했습니다.",signUpResponseDto));
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(ApiResponse.error("회원가입에 실패했습니다."));
+        }
+    }
+}
