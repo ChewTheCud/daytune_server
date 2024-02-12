2db57823286566ccaec1c378cadf026932e4cd00
2jaebbang <2jaebbang@naver.com>
2024-02-12 12:31:51 +0900

    feat: param값 diaryId로 시작하는 파일명의 파일url 가져오는 api 추가

--- src/main/java/com/eumakase/eumakase/controller/FileController.java
+++ src/main/java/com/eumakase/eumakase/controller/FileController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/api/v1/file")
 public class FileController {
@@ -28,5 +30,14 @@ public class FileController {
                     .status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(ApiResponse.error("파일 업로드에 실패했습니다."));
         }
+    }
+
+    @GetMapping("/download/urls/{diaryId}")
+    public ResponseEntity<List<String>> getDownloadUrlsByDiaryId(@PathVariable String diaryId) {
+        List<String> downloadUrls = fileService.getFileDownloadUrlsByDiaryId(diaryId);
+        if (downloadUrls.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(downloadUrls);
     }
 }
