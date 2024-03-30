f9dde07d5518a684aa5d33ede83821bd6a837ffe
2jaebbang <2jaebbang@naver.com>
2024-03-30 21:29:11 +0900

    fix: FCM config 수정

--- src/main/java/com/eumakase/eumakase/config/FirebaseConfig.java
+++ src/main/java/com/eumakase/eumakase/config/FirebaseConfig.java
@@ -3,9 +3,7 @@ package com.eumakase.eumakase.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.cloud.StorageClient;
-import jakarta.annotation.PostConstruct;
+import com.google.firebase.messaging.FirebaseMessaging;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -15,8 +13,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
 
-import java.beans.BeanProperty;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
@@ -29,19 +25,35 @@ public class FirebaseConfig {
     @Value("${firebase.bucket-name}")
     private String BUCKET_NAME;
 
-    @PostConstruct
-    public void initialize() {
+    @Bean
+    public FirebaseApp initializeFirebaseApp() throws IOException {
         try {
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(new ClassPathResource("daytune-3722b-firebase-adminsdk-ee8uq-3ac1946af3.json").getInputStream()))
                     .setStorageBucket(BUCKET_NAME)
                     .build();
-
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-            }
+            return FirebaseApp.initializeApp(options);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            log.error("Firebase 초기화 실패", e);
+            throw new RuntimeException("Firebase를 초기화할 수 없습니다.", e);
         }
+    }
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        return FirebaseMessaging.getInstance(initializeFirebaseApp());
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public HttpHeaders httpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        return headers;
     }
 }
