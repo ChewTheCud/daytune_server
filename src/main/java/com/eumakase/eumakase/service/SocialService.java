644a361c95398f9419bcc2a7eddda2fc263d22f2
2jaebbang <2jaebbang@naver.com>
2024-04-29 19:06:59 +0900

    fix: 애플 소셜로그인시 ClientSecret 생성로직 수정

--- src/main/java/com/eumakase/eumakase/service/SocialService.java
+++ src/main/java/com/eumakase/eumakase/service/SocialService.java
@@ -5,24 +5,15 @@ import com.eumakase.eumakase.config.KakaoProperties;
 import com.eumakase.eumakase.config.SocialConfig;
 import com.eumakase.eumakase.dto.auth.apple.AppleResponseDto;
 import com.eumakase.eumakase.dto.auth.kakao.KakaoResponseDto;
-import io.jsonwebtoken.JwtException;
-import org.bouncycastle.asn1.ASN1InputStream;
-import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.io.StringReader;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.time.Instant;
+import java.security.Security;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.util.Base64;
 import java.util.Date;
 
@@ -30,7 +21,7 @@ import io.jsonwebtoken.JwsHeader;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
-
+import org.springframework.web.client.RestClientException;
 
 /**
  * SNS에 대한 OAuth 토큰 검증 및 API 요청 수행
@@ -81,48 +72,53 @@ public class SocialService {
                 "&code=" + authorizationCode, headers);
 
         System.out.println("request body:" + request.getBody());
+        System.out.println("request:" + appleProperties.getClientId() + "/auth/token");
 
-        ResponseEntity<AppleResponseDto> response = socialConfig.restTemplate().exchange(
-                appleProperties.getClientId() + "/auth/token", HttpMethod.POST, request, AppleResponseDto.class);
-
-        System.out.println("test response:" + response);
-        return response.getBody();
-    }
-
-    public String generateClientSecret() {
         try {
-            LocalDateTime expiration = LocalDateTime.now(ZoneOffset.UTC).plusMinutes(5);
-            PrivateKey privateKey = getPrivateKey(); // 예외 발생 가능한 부분
-            System.out.println("privateKey:"+privateKey.toString());
-            return Jwts.builder()
-                    .setHeaderParam(JwsHeader.KEY_ID, appleProperties.getKeyId())
-                    .setIssuer(appleProperties.getTeamId())
-                    .setAudience(appleProperties.getAudience())
-                    .setSubject(appleProperties.getClientId())
-                    .setExpiration(Date.from(expiration.toInstant(ZoneOffset.UTC)))
-                    .setIssuedAt(Date.from(Instant.now()))
-                    .signWith(SignatureAlgorithm.ES256, privateKey)
-                    .compact();
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            throw new RuntimeException("Error retrieving private key: " + e.getMessage(), e);
-        } catch (JwtException e) {
-            System.out.println(e.getMessage());
-            throw new RuntimeException("Error creating JWT: " + e.getMessage(), e);
-        } catch (Exception e) {
-            // 포괄적 예외 처리
-            System.out.println(e.getMessage());
-            throw new RuntimeException("An unexpected error occurred: " + e.getMessage(), e);
-        }
+            ResponseEntity<AppleResponseDto> response = socialConfig.restTemplate().exchange(
+                    appleProperties.getAudience() + "/auth/token", HttpMethod.POST, request, AppleResponseDto.class);
+            System.out.println("test response:" + response);
+            return response.getBody();
+        } catch (RestClientException e) {
+        // RestClientException 처리
+        System.err.println("RestClientException occurred: " + e.getMessage());
+        // 적절한 예외 처리 로직을 여기에 추가하세요. 예를 들어, 로그 기록, 오류 응답 반환 등
+    } catch (Exception e) {
+        // 그 외 예외 처리
+        System.err.println("An unexpected error occurred: " + e.getMessage());
+        // 추가 예외 처리 로직
     }
 
-    private PrivateKey getPrivateKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-        String privateKeyPEM = appleProperties.getPrivateKey();
-        System.out.println("privateKeyPEM key: "+ privateKeyPEM);
-        byte[] encoded = Base64.getDecoder().decode(privateKeyPEM);
-        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
-        KeyFactory keyFactory = KeyFactory.getInstance("EC"); // ECDSA 키를 위한 인스턴스, RSA 등 다른 키 타입인 경우 변경 필요
-        System.out.println("privateKeyPEM key: "+ keyFactory.generatePrivate(keySpec));
-        return keyFactory.generatePrivate(keySpec);
+        return socialConfig.restTemplate().exchange(
+                appleProperties.getAudience() + "/auth/token", HttpMethod.POST, request, AppleResponseDto.class).getBody();
+    }
+
+    private String generateClientSecret() {
+        LocalDateTime expiration = LocalDateTime.now().plusMinutes(5);
+
+        return Jwts.builder()
+                .setHeaderParam(JwsHeader.KEY_ID, appleProperties.getKeyId())
+                .setIssuer(appleProperties.getTeamId())
+                .setAudience(appleProperties.getAudience())
+                .setSubject(appleProperties.getClientId())
+                .setExpiration(Date.from(expiration.atZone(ZoneId.systemDefault()).toInstant()))
+                .setIssuedAt(new Date())
+                .signWith(getPrivateKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    private PrivateKey getPrivateKey() {
+
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+
+        try {
+            byte[] privateKeyBytes = Base64.getDecoder().decode(appleProperties.getPrivateKey());
+
+            PrivateKeyInfo privateKeyInfo = PrivateKeyInfo.getInstance(privateKeyBytes);
+            return converter.getPrivateKey(privateKeyInfo);
+        } catch (Exception e) {
+            throw new RuntimeException("Error converting private key from String", e);
+        }
     }
 }
