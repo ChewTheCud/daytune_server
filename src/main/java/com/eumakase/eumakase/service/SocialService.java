cee5e290f2e37a17b98bf9586e489e984d1cea91
2jaebbang <2jaebbang@naver.com>
2024-04-29 18:34:43 +0900

    fix: 애플 소셜로그인시 ClientSecret 생성로직 수정

--- src/main/java/com/eumakase/eumakase/service/SocialService.java
+++ src/main/java/com/eumakase/eumakase/service/SocialService.java
@@ -5,13 +5,20 @@ import com.eumakase.eumakase.config.KakaoProperties;
 import com.eumakase.eumakase.config.SocialConfig;
 import com.eumakase.eumakase.dto.auth.apple.AppleResponseDto;
 import com.eumakase.eumakase.dto.auth.kakao.KakaoResponseDto;
+import io.jsonwebtoken.JwtException;
 import org.bouncycastle.asn1.ASN1InputStream;
+import org.bouncycastle.openssl.PEMParser;
 import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.io.StringReader;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -82,31 +89,40 @@ public class SocialService {
         return response.getBody();
     }
 
-    private String generateClientSecret() throws IOException {
-        LocalDateTime expiration = LocalDateTime.now(ZoneOffset.UTC).plusMinutes(5);
-        return Jwts.builder()
-                .setHeaderParam(JwsHeader.KEY_ID, appleProperties.getKeyId())
-                .setIssuer(appleProperties.getTeamId())
-                .setAudience(appleProperties.getAudience())
-                .setSubject(appleProperties.getClientId())
-                .setExpiration(Date.from(expiration.toInstant(ZoneOffset.UTC)))
-                .setIssuedAt(Date.from(Instant.now()))
-                .signWith(SignatureAlgorithm.ES256, getPrivateKey())
-                .compact();
+    public String generateClientSecret() {
+        try {
+            LocalDateTime expiration = LocalDateTime.now(ZoneOffset.UTC).plusMinutes(5);
+            PrivateKey privateKey = getPrivateKey(); // 예외 발생 가능한 부분
+            System.out.println("privateKey:"+privateKey.toString());
+            return Jwts.builder()
+                    .setHeaderParam(JwsHeader.KEY_ID, appleProperties.getKeyId())
+                    .setIssuer(appleProperties.getTeamId())
+                    .setAudience(appleProperties.getAudience())
+                    .setSubject(appleProperties.getClientId())
+                    .setExpiration(Date.from(expiration.toInstant(ZoneOffset.UTC)))
+                    .setIssuedAt(Date.from(Instant.now()))
+                    .signWith(SignatureAlgorithm.ES256, privateKey)
+                    .compact();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException("Error retrieving private key: " + e.getMessage(), e);
+        } catch (JwtException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException("Error creating JWT: " + e.getMessage(), e);
+        } catch (Exception e) {
+            // 포괄적 예외 처리
+            System.out.println(e.getMessage());
+            throw new RuntimeException("An unexpected error occurred: " + e.getMessage(), e);
+        }
     }
 
-    private PrivateKey getPrivateKey() throws IOException {
-        ASN1InputStream asn1InputStream = null;
-        try {
-            byte[] privateKeyBytes = Base64.getDecoder().decode(appleProperties.getPrivateKey());
-            asn1InputStream = new ASN1InputStream(privateKeyBytes);
-            PrivateKeyInfo privateKeyInfo = PrivateKeyInfo.getInstance(asn1InputStream.readObject());
-            JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
-            return converter.getPrivateKey(privateKeyInfo);
-        } catch (IllegalArgumentException | IOException e) {
-            throw new IllegalArgumentException("Failed to decode or convert private key", e);
-        } finally {
-            asn1InputStream.close();
-        }
+    private PrivateKey getPrivateKey() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        String privateKeyPEM = appleProperties.getPrivateKey();
+        System.out.println("privateKeyPEM key: "+ privateKeyPEM);
+        byte[] encoded = Base64.getDecoder().decode(privateKeyPEM);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
+        KeyFactory keyFactory = KeyFactory.getInstance("EC"); // ECDSA 키를 위한 인스턴스, RSA 등 다른 키 타입인 경우 변경 필요
+        System.out.println("privateKeyPEM key: "+ keyFactory.generatePrivate(keySpec));
+        return keyFactory.generatePrivate(keySpec);
     }
 }
