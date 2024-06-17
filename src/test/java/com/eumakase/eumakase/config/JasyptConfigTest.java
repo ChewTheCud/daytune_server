b04479b42a12fc76f7c11e52b4df9b5a8c57bb07
2jaebbang <2jaebbang@naver.com>
2024-06-17 19:22:58 +0900

    test: JasyptConfig 테스트

---
+++ src/test/java/com/eumakase/eumakase/config/JasyptConfigTest.java
@@ -0,0 +1,30 @@
+package com.eumakase.eumakase.config;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+class JasyptConfigTest {
+    @Test
+    void privateKeyEN() throws IOException {
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        standardPBEStringEncryptor.setAlgorithm(JasyptConfig.ALGORITHM);
+        standardPBEStringEncryptor.setPassword(JasyptConfig.KEY);
+
+        String enc = standardPBEStringEncryptor.encrypt("sk-proj-QgXNcgwfobADO0jWhXzFT3BlbkFJ2hUQrQrBvNsAiR4qPtQY");
+        System.out.printf("enc = ENC(%s)\\n", enc);
+        System.out.printf("dec = %s\\n", standardPBEStringEncryptor.decrypt(enc));
+    }
+
+    @Test
+    void stringDE() {
+        String privateKey = "cbejzjem2023!";
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        standardPBEStringEncryptor.setAlgorithm(JasyptConfig.ALGORITHM);
+        standardPBEStringEncryptor.setPassword(JasyptConfig.KEY);
+
+        String enc = standardPBEStringEncryptor.encrypt(privateKey);
+        System.out.printf("enc = ENC(%s)\\n", enc);
+        System.out.printf("dec = %s\\n", standardPBEStringEncryptor.decrypt(enc));
+    }
+}
